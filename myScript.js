d01e9ef2addeaa19a02d6d7fa0430ed8a00ae5c2
warheadz23 <53065381+warheadz23@users.noreply.github.com>
2025-01-26 21:53:33 +0800

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+        const slides = document.querySelectorAll('.hero img');
+        const totalSlides = slides.length;
+
+        function showSlide(index) {
+            slides.forEach((slide, i) => {
+                slide.classList.remove('active');
+                if (i === index) {
+                    slide.classList.add('active');
+                }
+            });
+        }
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % totalSlides;
+            showSlide(currentSlide);
+        }
+
+        setInterval(nextSlide, 3000);
